456cf59a8a00989b5be6122047600cc8092c2528
Collado69 <audrey.sauvageon@yahoo.fr>
2019-04-09 10:45:57 +0200

    jquery cnv

--- main.js
+++ main.js
@@ -1,13 +1,55 @@
 $(function(){
 
+    //stocker les analyse sur le texte
+    var analyse = {
+        "nombredemot": 0,
+        "jamais": false,
+        "toujours": false,
+        "nbEmotionWords": 0,
+        "nbNeedsWords": 0,
+        "nbInsults": 0,
+        "tu":0
+    }
+
+    //message à afficher à l'utilsiateur
+    var messages = [];
+
+    
     $("#buttonTry").click(function() {
-        var res = $("#exampleFormControlTextarea1").val().split(" ");
-        console.log(res[0]);
+        //récupérer contenu textArea
+        var parsedText = $("#exampleFormControlTextarea1").val().split(" ");
+        var cleanedParsedText = cleanText(parsedText);
+        // step1. count number of words
+        analyse.nombredemot = parsedText.length;
+        
+        //step2. look for dangerous words
+        //looking for "toujours"
+        analyse.toujours = true;
+        var monMessage = {
+            "type": "error",
+            "message": "Attention le mot toujours peut poser problème en CNV blablabli blablabla"
+        }
+        messages.push(monMessage);
+
+        //get template and update page for analyse
+        var tpl =$("#templateAnalyse").html();
+        html2generate = Mustache.render(tpl, analyse);
+        $("#inputAndResults").append(html2generate);
+
+        //get template and update page for messages
+        var tplErrorMsg =$("#messageErrorTpl").html();
+        for(i=0;i<messages.length;i++){
+            html2generate = Mustache.render(tplErrorMsg, messages[i]);
+            $("#messages").append(html2generate);
+        }
+     
+
+
  });
 
- // recuperer la phrase en entier 
  
- // parcourir le nombre de mot
+ 
+ // parcourir le text de mot dans le textarea
 
  // 
 
